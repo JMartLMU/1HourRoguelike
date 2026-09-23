682f2fbeb3b70e4ed2bd9d64bc446841a756c5b0
JMartLMU <jmart253@lion.lmu.edu>
2026-09-23 12:09:42 -0700

    shitty fucking thing

---
+++ 1HourRogueLike/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private BoardManager m_Board;
+    private Vector2Int m_CellPosition;
+    private bool m_IsGameOver;
+
+
+    public void Spawn(BoardManager boardManager, Vector2Int cell)
+    {
+        m_Board = boardManager;
+        m_IsGameOver = false;
+        MoveTo(cell);
+    }
+    public void GameOver()
+    {
+        m_IsGameOver = true;
+    }
+    public void MoveTo(Vector2Int cell)
+    {
+        m_CellPosition = cell;
+        transform.position = m_Board.CellToWorld(m_CellPosition);
+    }
+
+
+    private void Update()
+    {
+        if (m_Board == null)
+        {
+            return;
+        }
+
+        Vector2Int newCellTarget = m_CellPosition;
+        bool hasMoved = false;
+
+        if (m_IsGameOver)
+        {
+            if (Keyboard.current.enterKey.wasPressedThisFrame)
+            {
+                GameManager.Instance.StartNewGame();
+            }
+
+            return;
+        }
+
+        if(Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.y += 1;
+            hasMoved = true;
+        }
+        else if(Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.y -= 1;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.x += 1;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.x -= 1;
+            hasMoved = true;
+        }
+
+        if(hasMoved)
+        {
+            //check if the new position is passable, then move there if it is.
+            BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
+
+            if(cellData != null && cellData.Passable)
+            {
+                GameManager.Instance.TurnManager.Tick();
+                MoveTo(newCellTarget);
+            }
+        }
+    }
+
+}
